ad4d67d743bfcf78f9e4e6ca8429f47a3260c6bf
kyoung3212 <kevin.young3212@gmail.com>
2018-08-08 19:21:40 -0700

    completed prototype

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,13 @@
+import React from 'react';
+import MoviesContainer from './movie_container.js';
 
+const App = function(){
+    return(
+        <div className = "container">
+            <h1 className = "text-center">Axios Demo</h1>
+            <MoviesContainer/>
+        </div>
+    )
+}
+ export default App;
 
